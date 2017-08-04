b0429639eedb0f8c94550af13d0a2d509706d96f
Wisarut <noomerzx@gmail.com>
2017-08-04 19:17:47 +0700

    update noomerzx profile

--- src/views/Profile/Noomerzx/Index.vue
+++ src/views/Profile/Noomerzx/Index.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>noomerZx page</h1>
+  <div>
+    <img class="img-profile" src="https://scontent.fbkk5-8.fna.fbcdn.net/v/t1.0-9/10516820_10153124532530907_9197075119724828324_n.jpg?oh=90624775cfb07ec8815706c5ac9537c4&oe=5A354009" alt="">
+    <h1 class="head">Wisarut Phuvanantanond</h1>
+    <h2>noomerZx</h2>
+    <h2></h2>
+  </div>
 </template>
 
 <script>
@@ -7,4 +12,17 @@
   }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .img-profile {
+    margin-top: 50px;
+    border-radius: 50%;
+    max-width: 300px;
+  }
+  h1.head {
+    margin-top: 30px;
+    color: white;
+  }
+  h2 {
+    color: white;
+  }
+</style>
